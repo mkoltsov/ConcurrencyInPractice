23c587650d81c7ebe810e218c306ff2067921429
mkoltsov <root@javabean.ru>
2014-11-04 00:55:09 +0400

    volatile conforms to the happens-before principle

--- ImmutableVolatiles.groovy
+++ ImmutableVolatiles.groovy
@@ -3,31 +3,32 @@ class ImmutableHolder {
 
     ImmutableHolder(val) {
         this.value = val
-        println val
     }
 
-    int getValue() {
-        new Integer(value)
+    Integer getValue() {
+        if (value == null) {
+            null
+        } else {
+            new Integer(value)
+        }
     }
 }
 
 
 def imutHoldPenetrator = new Runnable() {
-    private volatile ImmutableHolder cache = new ImmutableHolder(0)
+    private volatile ImmutableHolder cache = new ImmutableHolder(null)
 
     @Override
     void run() {
         int val = new Random().nextInt(10)
-        println cache.getValue()
-        if (cache.getValue() == 0) {
-            cache = new ImmutableHolder(val)
-//            println "put $val to cache"/
+        synchronized (this) {
+            if (cache.getValue() == null) {
+                cache = new ImmutableHolder(val)
+                println "put $val to cache"
+            } else {
+                println "$val is in cache"
+            }
         }
-//        else {
-//
-//
-//
-//        }
     }
 }
 
