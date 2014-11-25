5327a125445198672e42e15132fcd8483d8840d1
mkoltsov <root@javabean.ru>
2014-11-25 23:39:14 +0300

    livelock example added, rearranged lock examples

--- Part II:Structuring Concurrent Applications/Chapter.10/Livelock.groovy
+++ Part II:Structuring Concurrent Applications/Chapter.10/Livelock.groovy
@@ -1,2 +1,84 @@
-//#TODO Implement
-//starting to implement
+import LiveLockExample.Spoon
+import groovy.transform.Canonical
+
+import java.util.concurrent.Executors
+
+class LiveLockExample {
+    static class Spoon {
+        private Diner owner
+
+        Spoon(diner) {
+            this.owner = diner
+        }
+
+        def getOwner() {
+            owner
+        }
+
+        public synchronized void setOwner(diner) {
+            this.owner = diner
+        }
+
+        public synchronized void use() {
+            println "has eaten $owner.name"
+        }
+    }
+
+    @Canonical
+    static class Diner {
+        def String name
+        def boolean isHungry
+
+        def void eatWith(Spoon spoon, Diner spouse) {
+            while (isHungry) {
+                //don't have a spoon, so wait patiently
+                if (spoon.owner != this) {
+                    sleep 1
+                    continue
+                }
+                //if spouse is hungry insist upon passing the spoon
+                if (spouse.isHungry) {
+                    println "eat first dear $name  = $spouse.name"
+                    spoon.setOwner(spouse)
+                }
+
+                //spouse wasn't hungry, so finally eat
+                spoon.use()
+                isHungry = false
+                println "i'm stuffed darling $name $spouse.name"
+                spoon.setOwner(spouse)
+            }
+        }
+    }
+}
+
+def husband = new LiveLockExample.Diner("Chef", true)
+def wife = new LiveLockExample.Diner("Pupa", true)
+
+final Spoon spoon = new Spoon(husband)
+
+Executors.newCachedThreadPool().with {
+    execute(new Runnable() {
+        @Override
+        void run() {
+            husband.eatWith(spoon, wife)
+            wife.eatWith(spoon, husband)
+        }
+    })
+}
+
+//new Thread((new Runnable() {
+//    @Override
+//    void run() {
+//        husband.eatWith(spoon, wife)
+//
+//    }
+//})).start()
+//
+//new Thread((new Runnable() {
+//    @Override
+//    void run() {
+//        wife.eatWith(spoon, husband)
+//
+//    }
+//})).start()
---
+++ Part II:Structuring Concurrent Applications/Chapter.13/ReentrantLocking.groovy
@@ -0,0 +1,38 @@
+import java.util.concurrent.Executors
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+class Task5 implements Runnable {
+    private final Lock lock
+
+    Task5(Lock lock) {
+        this.lock = lock
+    }
+
+    @Override
+    void run() {
+        lock.lock()
+        try {
+            println Thread.currentThread().getName() + " has acquired the lock"
+            sleep 500
+        } finally {
+            lock.unlock()
+        }
+    }
+}
+//fairness is not promised by the JMM!
+//Consider not using it
+def lock = new ReentrantLock(true)
+
+
+Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()+40).with {
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+    execute(new Task5(lock))
+}
